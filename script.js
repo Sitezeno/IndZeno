e1ed6d0b7391c903d0b3ec5f460a7f1ac39b8113
Sitezeno <lovab5487@gmail.com>
2026-07-12 14:12:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("INDZENO Loaded Successfully");
+
+document.getElementById("main-content").innerHTML = `
+<div class="bg-white rounded-2xl p-6 shadow">
+  <h2 class="text-2xl font-bold">Welcome to INDZENO</h2>
+  <p class="mt-2">Latest News Loading...</p>
+</div>
+`;
