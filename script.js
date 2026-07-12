f49d48e36e420709cd325d233b6113625d702c7e
Sitezeno <lovab5487@gmail.com>
2026-07-12 14:59:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,102 @@
-function openMenu(){
-document.getElementById("sidebar").classList.add("active");
-document.getElementById("overlay").classList.add("active");
+// ================= SEARCH =================
+
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".news-card");
+
+if(searchInput){
+searchInput.addEventListener("keyup", function(){
+
+let value = this.value.toLowerCase();
+
+cards.forEach(card => {
+
+let title = card.querySelector("h3").innerText.toLowerCase();
+
+if(title.includes(value)){
+card.style.display = "block";
+}else{
+card.style.display = "none";
 }
 
-function closeMenu(){
-document.getElementById("sidebar").classList.remove("active");
-document.getElementById("overlay").classList.remove("active");
+});
+
+});
 }
 
-function openSearch(){
-document.getElementById("searchBox").classList.toggle("active");
+
+// ================= ARTICLE MODAL =================
+
+const modal = document.getElementById("articleModal");
+const modalTitle = document.getElementById("modalTitle");
+const modalText = document.getElementById("modalText");
+const modalImg = document.getElementById("modalImg");
+
+document.querySelectorAll(".news-card").forEach(card=>{
+
+card.addEventListener("click",()=>{
+
+modal.style.display = "flex";
+
+modalTitle.innerText =
+card.dataset.title;
+
+modalText.innerText =
+card.dataset.content;
+
+modalImg.src =
+card.dataset.image;
+
+});
+
+});
+
+function closeModal(){
+modal.style.display = "none";
 }
 
-document.getElementById("overlay").onclick = closeMenu;
+
+// ================= BOOKMARK =================
+
+function saveBookmark(title){
+
+let bookmarks =
+JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+if(!bookmarks.includes(title)){
+
+bookmarks.push(title);
+
+localStorage.setItem(
+"bookmarks",
+JSON.stringify(bookmarks)
+);
+
+showToast("Saved Successfully");
+
+}else{
+
+showToast("Already Saved");
+
+}
+
+}
+
+
+// ================= TOAST =================
+
+function showToast(text){
+
+const toast =
+document.getElementById("toast");
+
+toast.innerText = text;
+
+toast.classList.add("show");
+
+setTimeout(()=>{
+
+toast.classList.remove("show");
+
+},3000);
+
+}
