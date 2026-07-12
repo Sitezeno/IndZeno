744e8bf575fdd533f3dd326c8383a4a5a341cf78
Sitezeno <lovab5487@gmail.com>
2026-07-12 17:27:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ document.getElementById("searchBox").classList.toggle("active");
 }
 
 document.getElementById("overlay").onclick = closeMenu;
+
+
+/* ===== SUPABASE ===== */
+
+const supabaseUrl = "https://ltknnbjsymyrpnuntwyo.supabase.co/rest/v1/";
+const supabaseKey = "sb_publishable_r2vMX1He-CTQk8yxKcGcOw__aaTWrLd";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+async function loadArticles() {
+  const { data, error } = await supabase
+    .from("articles")
+    .select("*")
+    .eq("published", true)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log("Articles:", data);
+}
+
+loadArticles();
