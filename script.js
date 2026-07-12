7375e90f2da74c809fc950763aeb5bff75e8a51a
Sitezeno <lovab5487@gmail.com>
2026-07-12 13:58:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 console.log("INDZENO Loaded Successfully");
-
-document.addEventListener("DOMContentLoaded", function(){
-
-const cards=document.querySelectorAll(".card");
-
-cards.forEach(card=>{
-card.addEventListener("click",()=>{
-card.style.transform="scale(1.03)";
-setTimeout(()=>{
-card.style.transform="scale(1)";
-},200);
-});
-});
-
-});
